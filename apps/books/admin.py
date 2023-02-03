547c49cb68c23d340c657d521cb47cde1b68384c
VitRod <ukrdao@gmail.com>
2023-02-03 18:40:57 +0200

    added new urls file

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from apps.books.models import Book
+from books.models import Book
 
 admin.site.register(Book)
--- apps/books/views.py
+++ apps/books/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-from apps.books.models import Book
+from books.models import Book
 
 class BookList(ListView):
     model = Book
@@ -26,3 +26,4 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('book_list')
+
